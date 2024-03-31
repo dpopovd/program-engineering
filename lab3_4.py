6406a86d612f1b8eef78288cadc63a1f9242a31c
DimaPopov <dimapopov9666@gmail.com>
2024-03-31 21:17:18 +0500

    F

---
+++ lab3_4.py
@@ -0,0 +1,17 @@
+     sentences = [
+        "London is the capital of the United Kingdom",
+        "Oh my god, I'm so tired of doing these DIY jobs...",
+        "He used to think he was ugly, but now he sees the beauty in herself. End"
+    ]
+    vowels = "aeiou"
+    for sentence in sentences:
+        print("Исходное предложение", sentence)
+        print("Длина предложения:", len(sentence))
+        print("Предложение в нижнем регистре:", sentence.lower())
+        print("Количество гласных:", sum(1 for char in sentence if char.lower() in vowels))
+        print("Замените слово «уродливое»(ugly) на «красота»(beauty)", sentence.replace("ugly", "beauty"))
+        print("Начинается с «The»", sentence.startswith("The"))
+        print("Заканчивается словом «End»:", sentence.endswith("End"))
+        print()
+if __name__ == "__main__":
+    main()
